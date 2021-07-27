1ff202721dfc7ae44b6cff3a29ccbb5b1bb03662
unknown <olivierakesso@gmail.com>
2021-07-27 02:20:49 +0200

    mandatory's lab iterations done

--- app.js
+++ app.js
@@ -19,7 +19,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index.hbs');
+});
+
+// beers route.
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      const data = { beersFromApi: beersFromApi };
+      res.render('beers.hbs', data);
+      console.log(data);
+    })
+    .catch(error => console.log(error));
+});
+
+// random-beers route.
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beersFromApi => {
+      const data = { beersFromApi: beersFromApi };
+      res.render('random-beer.hbs', data);
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
